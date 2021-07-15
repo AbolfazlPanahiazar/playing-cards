a4471e15784ee11976523be155c054e5c995ec7b
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-07-16 01:35:04 +0430

    Read data of []byte from body and convert it into string

--- main.go
+++ main.go
@@ -12,5 +12,10 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	// fmt.Println(resp)
+
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+
+	fmt.Println(string(bs))
 }
