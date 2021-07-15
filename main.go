82aebc0ead486d589b0bb58cc12ce2b97628d05b
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-07-16 03:04:33 +0430

    Use channels for the first time

--- main.go
+++ main.go
@@ -14,16 +14,29 @@ func main() {
 		"http://tenzumusic.com",
 	}
 
+	// create a channel
+	c := make(chan string)
+
+	// thread go routine
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	// recieve a vlue from the channel
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + " might be down!")
+		c <- "Might be down i think"
 		return
 	}
 	fmt.Println(link + " is up!")
+	c <- "Yep its up"
 }
