52cdc3dcb5389608736cc843141a2e033383beda
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-07-16 01:59:46 +0430

    Add my first Writer function

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com/")
 	if err != nil {
@@ -19,5 +21,15 @@ func main() {
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
 
-	io.Copy(os.Stdout, resp.Body)
+	// io.Copy(os.Stdout, resp.Body) // io.Copy(Writer inteface, Reader interface)
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body) // io.Copy(Writer inteface, Reader interface)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
